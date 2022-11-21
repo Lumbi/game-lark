e8ce912a751ac20b6957f30d3cab4a53e1cdfe4e
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2022-11-20 20:39:34 -0500

    Improve message display to avoid jumping alignment

--- Lark Shared/UI/Message/MessageViewController.swift
+++ Lark Shared/UI/Message/MessageViewController.swift
@@ -42,13 +42,13 @@ class MessageViewController: UIViewController {
         nextMessageIcon?.isHidden = true
         
         DispatchQueue.global(qos: .userInteractive).async {
-            var current = message.text.startIndex
-            while current < message.text.endIndex {
-                let text = String(message.text[...current])
-                let next = message.text.index(after: current)
+            let count = message.text.count
+            var current = 0
+            while current <= count {
+                let next = current + 1
                 DispatchQueue.main.async {
-                    self.messageLabel?.text = text
-                    let isCompleted = next >= message.text.endIndex
+                    self.updateText(with: message, showUntil: current)
+                    let isCompleted = next >= count
                     if isCompleted {
                         self.view.isUserInteractionEnabled = true
                         self.nextMessageIcon?.isHidden = false
@@ -58,6 +58,15 @@ class MessageViewController: UIViewController {
                 current = next
             }
         }
+    }
+    
+    private func updateText(with message: Message, showUntil endIndex: Int) {
+        let attributedString = NSMutableAttributedString(string: message.text)
+        let visibleRange = NSRange(location: 0, length: endIndex)
+        let hiddenRange = NSRange(location: endIndex, length: message.text.count - endIndex)
+        attributedString.addAttribute(.foregroundColor, value: UIColor.black, range: visibleRange)
+        attributedString.addAttribute(.foregroundColor, value: UIColor.clear, range: hiddenRange)
+        messageLabel?.attributedText = attributedString
     }
     
     @IBAction func didTap() {
