cea0d1e84507a274d9e74f9f4bab137832618352
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2022-12-03 22:19:41 -0500

    Implement disabling/enabled locked/unlocked levels

---
+++ Lark Shared/Services/ProgressService.swift
@@ -0,0 +1,46 @@
+//
+//  ProgressService.swift
+//  Lark iOS
+//
+//  Created by Gabriel Lumbi on 2022-12-03.
+//
+
+import Foundation
+
+enum LevelName: String {
+    case w1_l1 = "1-1"
+    case w1_l2 = "1-2"
+    case w1_l3 = "1-3"
+}
+
+enum LevelProgress: String {
+    case locked = "locked"
+    case unlocked = "unlocked"
+    case completed = "completed"
+}
+
+class ProgressService {
+    private let userDefaults: UserDefaults = .init()
+
+    func progress(for levelName: LevelName) -> LevelProgress {
+        if let rawValue = userDefaults.string(forKey: qualifiedKey(for: levelName.rawValue)),
+           let progress = LevelProgress(rawValue: rawValue)
+        {
+            return progress
+        } else {
+            if levelName == LevelName.w1_l1 { // Special case for level
+                return .unlocked
+            } else {
+                return .locked
+            }
+        }
+    }
+
+    func complete(levelName: LevelName) {
+        userDefaults.set(LevelProgress.completed.rawValue, forKey: qualifiedKey(for: levelName.rawValue))
+    }
+
+    private func qualifiedKey(for key: String) -> String {
+        "\(Bundle.main.bundleIdentifier!).\(Self.self).\(key)"
+    }
+}
--- Lark Shared/UI/LevelSelect/LevelSelectViewController.swift
+++ Lark Shared/UI/LevelSelect/LevelSelectViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 import SpriteKit
 
 class LevelSelectViewController: UIViewController {
+    var progressService: ProgressService = .init()
+
+    @IBOutlet weak var selectLevel1Button: UIButton?
+    @IBOutlet weak var selectLevel2Button: UIButton?
+    @IBOutlet weak var selectLevel3Button: UIButton?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        updateLevelButtonsAvailability()
     }
 
     @IBAction func tapSelectLevel1() {
@@ -40,5 +52,41 @@ class LevelSelectViewController: UIViewController {
                 self.view.window?.rootViewController = viewController
             }
         )
+    }
+
+    private func updateLevelButtonsAvailability() {
+        if let selectLevel1Button = selectLevel1Button,
+           let selectLevel2Button = selectLevel2Button,
+           let selectLevel3Button = selectLevel3Button
+        {
+            updateLevelButtonAvailability(
+                for: selectLevel1Button,
+                progress: progressService.progress(for: .w1_l1)
+            )
+
+            updateLevelButtonAvailability(
+                for: selectLevel2Button,
+                progress: progressService.progress(for: .w1_l2)
+            )
+
+            updateLevelButtonAvailability(
+                for: selectLevel3Button,
+                progress: progressService.progress(for: .w1_l3)
+            )
+        }
+    }
+
+    private func updateLevelButtonAvailability(for button: UIButton, progress: LevelProgress) {
+        switch progress {
+        case .locked:
+            button.isEnabled = false
+            button.tintColor = .blue
+        case .unlocked:
+            button.isEnabled = true
+            button.tintColor = .blue
+        case .completed:
+            button.isEnabled = true
+            button.tintColor = .green
+        }
     }
 }
