51d079d4a193fbdcdea3d164b1048bfa625fec09
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2022-12-29 21:22:05 -0500

    Add cooldown and dashing timer to dash control script

--- Assets/PlayerDashControl.cs
+++ Assets/PlayerDashControl.cs
@@ -8,12 +8,21 @@ public class PlayerDashControl : MonoBehaviour
     public float minDistance = 1f;
     public float minInputSpeed = 1f;
     public float maxInputDuration = 0.3f;
+    public float cooldownDuration = 1f;
+    public Behaviour controls; // Normal player controls to be disabled while dashing
+
     private float inputDuration = 0f;
+    private bool inCooldown = false;
+    private float cooldownTimer = 0f;
+
+    private bool triggerDash = false;
     private Vector2 dashDirection = Vector2.zero;
+    private bool isDashing = false;
+    private float dashDuration = 0.3f;
+    private float dashTimer = 0f;
+
     private Rigidbody2D body;
     private PlayerInput input;
-
-    private bool applyForce = false;
 
     void Start()
     {
@@ -23,26 +32,60 @@ public class PlayerDashControl : MonoBehaviour
 
     void Update()
     {
-        if (input.IsDown() && !applyForce) {
+        // Accumulate cooldown time
+        if (inCooldown) {
+            if (cooldownTimer >= cooldownDuration) {
+                inCooldown = false;
+            } else {
+                cooldownTimer += Time.deltaTime;
+            }
+        } else {
+            cooldownTimer = 0f;
+        }
+
+        // Detect dash input
+        if (input.IsDown() && !inCooldown) {
             if (inputDuration < maxInputDuration) {
-                Debug.Log(input.Velocity().magnitude);
+                // Trigger dash
                 if (input.Distance().magnitude > minDistance && input.Velocity().magnitude > minInputSpeed) {
-                    Debug.Log("PUSH!");
-                    applyForce = true;
                     dashDirection = input.Velocity().normalized;
+                    triggerDash = true;
+                } else {
+                    inputDuration += Time.deltaTime;
                 }
-                inputDuration += Time.deltaTime;
             }
         } else {
             inputDuration = 0f;
+        }
+
+        // Accumulate dash state frames
+        if (isDashing) {
+            controls.enabled = false;
+            if (dashTimer >= dashDuration) {
+                isDashing = false;
+            } else {
+                dashTimer += Time.deltaTime;
+            }
+        } else {
+            dashTimer = 0f;
+            controls.enabled = true;
         }
     }
 
     void FixedUpdate()
     {
-        if (applyForce) {
-            applyForce = false;
-            body.AddForce(dashForce * dashDirection, ForceMode2D.Impulse);
+        if (triggerDash && !inCooldown) {
+            ApplyForce();
         }
+    }
+
+    public bool IsDashing() { return isDashing; }
+
+    private void ApplyForce()
+    {
+        triggerDash = false;
+        inCooldown = true;
+        cooldownTimer = 0f;
+        body.AddForce(dashForce * dashDirection, ForceMode2D.Impulse);
     }
 }
