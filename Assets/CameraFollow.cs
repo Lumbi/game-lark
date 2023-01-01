d4f9b316584335caee3e594edd1f165f88c8233b
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-01-01 16:41:59 -0500

    Fix the camera to predict target position and show ahead

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject target;
-    public float speed = 2.0f;
+    public float speed = 1.0f;
+    public float targetPredictTime = 1.0f;
 
     void LateUpdate()
     {
         if (target != null) {
             float interpolation = speed * Time.deltaTime;
+            Vector3 targetPosition = target.transform.position;
+            Rigidbody2D targetBody = target.GetComponent<Rigidbody2D>();
+
+            if (targetBody) {
+                Vector3 delta = targetBody.velocity * targetPredictTime;
+                targetPosition += delta;
+            }
+
             var position = this.transform.position;
-            position.y = Mathf.Lerp(this.transform.position.y, target.transform.position.y, interpolation);
-            position.x = Mathf.Lerp(this.transform.position.x, target.transform.position.x, interpolation);
+            position.y = Mathf.Lerp(this.transform.position.y, targetPosition.y, interpolation);
+            position.x = Mathf.Lerp(this.transform.position.x, targetPosition.x, interpolation);
 
             position.z = -10;
             this.transform.position = position;
