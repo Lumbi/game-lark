3e98f84fa6eca8674c8b70ff0b16e1acc7ce44b5
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-01-03 13:46:51 -0500

    Improve CameraFollow script using SmoothDamp

--- Assets/Camera/CameraFollow.cs
+++ Assets/Camera/CameraFollow.cs
@@ -5,27 +5,26 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject target;
-    public float speed = 1.0f;
-    public float targetPredictTime = 1.0f;
+    public float targetPredictDistance = 3.0f;
+    public float followSmoothTime = 0.5f;
+    private Vector2 velocity = Vector2.zero;
+    private Vector3 nextPosition = Vector3.zero;
 
     void LateUpdate()
     {
         if (target != null) {
-            float interpolation = speed * Time.deltaTime;
-            Vector3 targetPosition = target.transform.position;
+            Vector2 currentPosition = this.transform.position;
+            Vector2 targetPosition = target.transform.position;
             Rigidbody2D targetBody = target.GetComponent<Rigidbody2D>();
 
             if (targetBody) {
-                Vector3 delta = targetBody.velocity * targetPredictTime;
+                Vector2 delta = targetBody.velocity.normalized * targetPredictDistance;
                 targetPosition += delta;
             }
 
-            var position = this.transform.position;
-            position.y = Mathf.Lerp(this.transform.position.y, targetPosition.y, interpolation);
-            position.x = Mathf.Lerp(this.transform.position.x, targetPosition.x, interpolation);
-
-            position.z = -10;
-            this.transform.position = position;
+            nextPosition = Vector2.SmoothDamp(currentPosition, targetPosition, ref velocity, followSmoothTime);
+            nextPosition.z = -10;
+            this.transform.position = nextPosition;
         }
     }
 
