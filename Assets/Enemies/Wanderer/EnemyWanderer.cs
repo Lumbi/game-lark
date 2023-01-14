d31047ac709e2328f62d428b197f92296df08f48
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-01-13 22:21:31 -0500

    Wanderer kill player on touch

--- Assets/Enemies/Wanderer/EnemyWanderer.cs
+++ Assets/Enemies/Wanderer/EnemyWanderer.cs
@@ -10,4 +10,18 @@ public class EnemyWanderer : MonoBehaviour
     public Light2D leftEyeLight;
     public Light2D rightEyeLight;
     public SpriteRenderer spriteRenderer;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            KillPlayer(collision.gameObject);
+        }
+    }
+
+    private void KillPlayer(GameObject player)
+    {
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), player.GetComponent<Collider2D>());
+        player.GetComponent<LanderDestroy>().DestroyAndRespawn();
+        // TODO: Show FX
+    }
 }
