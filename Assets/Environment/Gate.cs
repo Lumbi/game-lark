71af8a434cc941f75b608cd3a629fd6776b73370
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-02-11 20:55:47 -0500

    Modify Gate API to allow open/close in editor

--- Assets/Environment/Gate.cs
+++ Assets/Environment/Gate.cs
@@ -2,15 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class Gate : MonoBehaviour
 {
+    public bool isOpen = false;
+    private bool wasOpen = false;
+
     public void Open()
     {
-        gameObject.SetActive(false);
+        Set(true);
     }
 
     public void Close()
     {
-        gameObject.SetActive(true);
+        Set(false);
+    }
+
+    void Update()
+    {
+        if (isOpen != wasOpen) {
+            if (isOpen) {
+                Open();
+            } else {
+                Close();
+            }
+        }
+
+        wasOpen = isOpen;
+    }
+
+    private void Set(bool open)
+    {
+        isOpen = open;
+        GetComponent<SpriteRenderer>().enabled = !open;
+        GetComponent<Collider2D>().enabled = !open;
+    }
+
+    void OnDrawGizmos()
+    {
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        Gizmos.color = spriteRenderer.color;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
     }
 }
